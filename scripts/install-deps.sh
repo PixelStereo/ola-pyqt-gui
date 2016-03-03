b5859dc8cde3b4b9fc56a7609cdf569d4ca2be40
reno- <contact@rubiano.fr>
2016-03-03 16:26:31 +0100

    build ola (prepare for embed)

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -11,17 +11,26 @@ case "$TRAVIS_OS_NAME" in
     export PATH="$HOME/.linuxbrew/bin:$PATH"
     export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"
     export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"
+    # install deps for OLA
+    sudo apt-get install -y libcppunit-dev libcppunit-1.13-0 uuid-dev pkg-config libncurses5-dev libtool autoconf automake g++ libmicrohttpd-dev \
+    libmicrohttpd10 protobuf-compiler libprotobuf-lite8 python-protobuf libprotobuf-dev libprotoc-dev zlib1g-dev bison flex make libftdi-dev  \
+    libftdi1 libusb-1.0-0-dev liblo-dev libavahi-client-dev
+
     echo "END LINUX"
    ;;
   osx)
     echo "START OSX"
-    echo "nothing to do… brew is already installed on osx"
+    sudo brew install pkgconfig cppunit protobuf-cpp libmicrohttpd libusb py27-protobuf
     echo "END OSX"
   ;;
 esac
 echo "START install-deps"
+git clone https://github.com/OpenLightingProject/ola.git ola
+autoreconf -i
+./configure --with-python
+make
+sudo make install
 brew install python
 brew link --overwrite python
 sudo pip install pyinstaller
-#sudo pip3 install pyinstaller
 echo "END install-deps"
