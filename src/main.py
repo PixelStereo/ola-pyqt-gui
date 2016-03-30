6a247e4ade0504929b94e0bab71d9c96cf132f84
reno- <contact@rubiano.fr>
2016-03-30 02:06:28 +0200

    import qdarkstyle only if available

--- src/main.py
+++ src/main.py
@@ -22,8 +22,11 @@ from Ola import OLA
 from universe import Universe
 from universe import UniversesModel
 from patch import PatchPanel
-# stylesheet
-import qdarkstyle
+try:
+    # stylesheet
+    import qdarkstyle
+except:
+    pass
 
 debug = 1
 
@@ -273,7 +276,10 @@ if __name__ == "__main__":
     root = QFileInfo(__file__).absolutePath()
     path = root+'/icon/icon.png'
     app.setWindowIcon(QIcon(path))
-    app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    try:
+        app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
+    except:
+        pass
     # create the Main Window and display it
     window = MainWindow()
     sys.exit(app.exec_())
